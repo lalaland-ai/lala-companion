87b622ddf9c47f02273cfab4a606f045849b202e
Christopher Trimboli <cjft@trimboli.io>
2024-01-08 15:22:52 -0700

    fix animations, bump deps

--- src/components/VRMCompanion.tsx
+++ src/components/VRMCompanion.tsx
@@ -26,20 +26,23 @@ const vrms = [
     scale: [1, 1, 1],
     animations: {
       idle: [
-        "https://lalaland.chat/animations/female/idle-1.fbx",
-        "https://lalaland.chat/animations/female/idle-2.fbx",
+        "https://lalaland.chat/animations/idle/idle-1.fbx",
+        "https://lalaland.chat/animations/idle/idle-2.fbx",
+        "https://lalaland.chat/animations/idle/idle-3.fbx",
+        "https://lalaland.chat/animations/idle/idle-4.fbx",
       ],
       greet: [
-        "https://lalaland.chat/animations/female/greet-1.fbx",
-        "https://lalaland.chat/animations/female/salute.fbx",
-        "https://lalaland.chat/animations/female/cat-ass.fbx",
+        "https://lalaland.chat/animations/greet/greet-1.fbx",
+        "https://lalaland.chat/animations/bored/female-pose-standing.fbx",
+        "https://lalaland.chat/animations/bored/rumba-dance.fbx",
+        "https://lalaland.chat/animations/bored/ymca.fbx",
+        "https://lalaland.chat/animations/bored/yawn.fbx",
       ],
       talk: [
-        "https://lalaland.chat/animations/female/talk-1.fbx",
-        "https://lalaland.chat/animations/female/talk-2.fbx",
-        "https://lalaland.chat/animations/female/talk-3.fbx",
+        "https://lalaland.chat/animations/talk/talk-1.fbx",
+        "https://lalaland.chat/animations/talk/talk-2.fbx",
+        "https://lalaland.chat/animations/talk/talk-3.fbx",
       ],
-      bored: ["https://lalaland.chat/animations/female/cat-ass.fbx"],
     },
   },
 ];
