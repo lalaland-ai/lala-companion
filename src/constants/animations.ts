e8ca58a5ad18db426ecf5a61413098ff80d3fa9e
Christopher Trimboli <cjft@trimboli.io>
2024-02-09 21:13:35 -0700

    new VRMCompanion, mouth lip audio sync, Scene cleanup

---
+++ src/constants/animations.ts
@@ -0,0 +1,44 @@
+export const animations = {
+  idle: [
+    "https://lalaland.chat/animations/idle/idle-1.fbx",
+    "https://lalaland.chat/animations/idle/idle-2.fbx",
+    "https://lalaland.chat/animations/idle/idle-3.fbx",
+    "https://lalaland.chat/animations/idle/idle-4.fbx",
+  ],
+  greet: [
+    "https://lalaland.chat/animations/greet/greet-1.fbx",
+    "https://lalaland.chat/animations/bored/ymca.fbx",
+    "https://lalaland.chat/animations/bored/rumba-dance.fbx",
+    "https://lalaland.chat/animations/talk/talk-1.fbx",
+    "https://lalaland.chat/animations/talk/talk-2.fbx",
+    "https://lalaland.chat/animations/talk/talk-3.fbx",
+    "https://lalaland.chat/animations/bored/yawn.fbx",
+  ],
+  talk: [
+    "https://lalaland.chat/animations/talk/talk-1.fbx",
+    "https://lalaland.chat/animations/talk/talk-2.fbx",
+    "https://lalaland.chat/animations/talk/talk-3.fbx",
+  ],
+  bored: ["https://lalaland.chat/animations/bored/yawn.fbx"],
+  walk: [
+    "https://lalaland.chat/animations/walk/walk-1.fbx",
+    "https://lalaland.chat/animations/walk/walk-2.fbx",
+  ],
+  happy: [
+    "https://lalaland.chat/animations/emotions/happy/happy-1.fbx",
+    "https://lalaland.chat/animations/emotions/happy/happy-2.fbx",
+    "https://lalaland.chat/animations/emotions/happy/happy-3.fbx",
+    "https://lalaland.chat/animations/emotions/happy/happy-4.fbx",
+  ],
+  angry: [
+    "https://lalaland.chat/animations/emotions/angry/angry-1.fbx",
+    "https://lalaland.chat/animations/emotions/angry/angry-2.fbx",
+    "https://lalaland.chat/animations/emotions/angry/angry-3.fbx",
+    "https://lalaland.chat/animations/emotions/angry/angry-4.fbx",
+  ],
+  sad: [
+    "https://lalaland.chat/animations/emotions/sad/sad-1.fbx",
+    "https://lalaland.chat/animations/emotions/sad/sad-2.fbx",
+    "https://lalaland.chat/animations/emotions/sad/sad-3.fbx",
+  ],
+};
